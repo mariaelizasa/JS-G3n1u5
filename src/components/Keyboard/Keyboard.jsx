5b13d4cb5e37df4ea51308be521076dc1cc5d11e
mariaelizasa <eliza.sads@gmail.com>
2021-02-14 19:44:47 -0300

    feat: creating game logic

--- src/components/Keyboard/Keyboard.jsx
+++ src/components/Keyboard/Keyboard.jsx
@@ -1,27 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Cell, Board, Display } from "./style.js";
 
 function Keyboard() {
   const emptyBoard = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  const [order, setorder] = useState();
-  const [hide, setHide] = useState(false);
+  const [round, setRound] = useState(false);
+  const [sorteado, setSorteado] = useState([]);
+  const [clicados, setClicados] = useState([]);
+  const [numeroDaVez, setNumeroDaVez] = useState(0);
 
-  const handleClick = (value) => {
-    console.log(value, "clicado");
+  let numberSorted;
+
+  useEffect(() => {
+    delay(2000).then(() => {
+      numberSorted = RandomNumber();
+      exibeNumeros();
+      console.log(exibeNumeros(), "aa");
+
+    });
+  }, [numberSorted]);
+
+  const handleClick = (value) => {};
+  const RandomNumber = () => {
+    let random = Math.floor(Math.random() * 9 + 1);
+    setSorteado(() => [...sorteado, random]);
+    return random;
   };
 
-  const sorteia = () => {
-    return Math.floor(Math.random() * 9 + 1);
+  
+  // passar para a pagina
+
+  const exibeNumeros = () => {
+    for (let i = 0; i < sorteado.length; i++) {
+      delay(300).then(() => {
+        setNumeroDaVez(sorteado[i]);
+      });
+    }
+  };
+
+  const delay = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   };
 
   return (
     <>
       <Display>
-        <p>{sorteia()}</p>
+        <p>{numeroDaVez}</p>
       </Display>
       <Board>
         {emptyBoard.map((value, index) => (
-          <Cell key={index} onClick={() => handleClick(value)}>
+          <Cell key={index} onClick={() => setClicados([...clicados, value])}>
             {value}
           </Cell>
         ))}
